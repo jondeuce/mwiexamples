008b01b94d7fdb7190d2609ce417e893684417c3
jondeuce <20175323+jondeuce@users.noreply.github.com>
2020-06-03 00:56:36 -0700

    update automatic installation of DECAES

--- decaes.jl
+++ decaes.jl
@@ -1,18 +1,14 @@
 # Load the Julia package manager, which is itself a Julia package
 using Pkg
 
-# Check if DECAES.jl has already been installed
-if !in("DECAES", keys(Pkg.installed()))
-    # Install DECAES.jl from the github repository
-    Pkg.add(
-        PackageSpec(
-            url = "https://github.com/jondeuce/DECAES.jl.git"
-        )
-    )
+try
+    # Load DECAES
+    @eval using DECAES
+catch e
+    # DECAES not found; install DECAES and try loading again
+    Pkg.add("DECAES")
+    @eval using DECAES
 end
-
-# Load DECAES.jl
-using DECAES
 
 # Call the command line interface entrypoint function
 main()
